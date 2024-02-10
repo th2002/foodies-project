56715ca203cc63664505f906347e1f0c05aba63c
Đinh Lê Trung Hiếu <dinhletrunghieu0207@gmail.com>
2024-02-10 12:52:45 +0700

    feat: create a few basic routes

---
+++ app/meals/share/page.js
@@ -0,0 +1,7 @@
+export default function ShareMealPage() {
+  return (
+    <main>
+      <h1>Share page</h1>
+    </main>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
+      <h1 style={ { color: 'white', textAlign: 'center' } }>
         Time to get started!
       </h1>
+      <p><Link href="/meals">Meals Page</Link></p>
+      <p><Link href="/meals/share">Share Meal Page</Link></p>
+      <p><Link href="/community">Community Page</Link></p>
     </main>
   );
 }
