f41e24d4b2cfb4c3c6a6bb9487cca11383f4d746
Đinh Lê Trung Hiếu <dinhletrunghieu0207@gmail.com>
2024-02-10 14:49:30 +0700

    style(main-header): optimize img, style logo and nav

--- src/components/main-header.js
+++ src/components/main-header.js
@@ -1,15 +1,17 @@
 import Link from 'next/link';
-import logoImg from '@/assets/logo.png'
+import Image from 'next/image';
 
+import logoImg from '@/assets/logo.png';
+import classes from './main-header.module.css';
 export default function MainHeader() {
   return (
-    <header>
-      <Link href="/">
-        <img src={logoImg.src} alt="A plate with food on it"/>
+    <header className={classes.header}>
+      <Link className={classes.logo} href="/">
+        <Image src={logoImg} alt="A plate with food on it" priority quality={100}/>
         NextLevel food
       </Link>
 
-      <nav>
+      <nav className={classes.nav}>
         <ul>
           <li>
             <Link href="/meals">Browse Meals</Link>
