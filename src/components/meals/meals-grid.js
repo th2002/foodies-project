0edbd9b1d0409cb1b51f6ccf4051c8b0497664ab
Đinh Lê Trung Hiếu <dinhletrunghieu0207@gmail.com>
2024-02-10 17:37:00 +0700

    feat(meals-grid,meal-item): add childred component of meal page

---
+++ src/components/meals/meals-grid.js
@@ -0,0 +1,15 @@
+import classes from './meals-grid.module.css'
+import MealItem from '@/components/meals/meal-item';
+export default function MealsGrid({meals}) {
+  return (
+    <ul className={classes.meals}>
+      {meals.map(meal => {
+        return (
+          <li key={ meal.id }>
+            <MealItem {...meal}/>
+          </li>
+        )
+      }) }
+    </ul>
+  )
+}
